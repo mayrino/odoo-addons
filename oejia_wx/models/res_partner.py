d8667275a69a7d74c62dfe08fe4e07c9e0182f12
mayrino <reyesxie@foxmail.com>
2020-04-29 22:25:57 +0800

    delete some incompatible website modules for odoo12

--- oejia_wx/models/res_partner.py
+++
@@ -1,9 +0,0 @@
-# coding=utf-8
-
-from openerp import models, fields, api
-
-
-class res_partner(models.Model):
-    _inherit = 'res.partner'
-    
-    wxcorp_user_id = fields.Many2one('wx.corpuser','关联企业号用户')
--- theme_mj_zoom_tri/README.md
+++
@@ -1,3 +0,0 @@
-# Transparent Navigation Menu
-
-a wiget module of website  builder  to  customize  iamge over showdow effitive!
